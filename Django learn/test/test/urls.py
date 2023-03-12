bf6fba290ceae6787d8f532ced8270a0555cf0ce
xipper <864353360@qq.com>
2023-03-12 21:18:47 +0800

    use template

--- Django learn/test/test/urls.py
+++ Django learn/test/test/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from hello.views import hello_world
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hello/', hello_world),
 ]
