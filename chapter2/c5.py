f3c6bcf8478d30af04cfc48eb04bc9e900bfd22e
wukunyun <864353360@qq.com>
2023-08-22 16:01:41 +0800

    rabbit turtle

--- chapter2/c5.py
+++ chapter2/c5.py
@@ -1,45 +1,32 @@
-"""
-使用装饰器统计兔子函数和乌龟函数的运行时间
-
-兔子每跑5米则随机休息1到10秒
-乌龟每跑1米则固定休息1秒
-
-最后统计兔子函数和乌龟函数的运行时间
-
-"""
-# 导入一个随机的工具， 这里是导入随机模块
+import  time
 import random
-# 导入时间工具
-import time
-
-# 定义一个跑道的长度
-track_length = 10
-# 定义装饰器
-
-def runtime_log(type):
-    def runtime(func):
-        def inner():
-            print("{}开始奔跑了".format(type))
+def run(type):
+    def start(func):
+        def runer():
             start_time = time.time()
             func()
             end_time = time.time()
-            print("<{}>的运行时间是:{:.2f}".format(type,end_time-start_time))
-        return inner
-    return runtime
+            print("{} takes {:.2f}".format(type, end_time - start_time))
+        return runer
+    return start
 
 
-@runtime_log("乌龟")
-def wugui():
-    for i in range(1,track_length+1):
-        print("乌龟跑了{}米".format(i))
+
+
+@run('turtle')
+def turtle():
+    for i in range(1, 10):
+        print("turtle run {} m".format(i))
         time.sleep(1)
 
-@runtime_log("兔子")
-def tuzi():
-    for i in range(1,track_length+1):
-        if i % 5 == 0:
-            time.sleep(random.randint(1,15))
-        print("兔子跑了{}米".format(i))
 
-wugui()
-tuzi()
+turtle()
+
+@run('rabbit')
+def rabbit():
+    for i in range(1, 10):
+        print("rabbit run {} m".format(i))
+        if i % 2 == 0:
+            time.sleep(random.randint(0, 3))
+
+rabbit()
